7c6e5d86df708caec17a3e125d16131dc89f987c
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2022-10-14 22:13:29 -0500

    Add files via upload

---
+++ MascotasUN/src/main/java/gestionVentas/Envio.java
@@ -0,0 +1,43 @@
+package gestionVentas;
+
+public class Envios {
+    private long envioID;
+    private String destino;;
+    private String estadoEnvio;
+    private int periodicidad;
+
+    
+    public Envios(long envioID, String destino, String estadoEnvio, int periodicidad) {
+        this.envioID = envioID;
+        this.destino = destino;
+        this.estadoEnvio = estadoEnvio;
+        this.periodicidad = periodicidad;
+    }
+    
+    public long getEnvioID() {
+        return envioID;
+    }
+    public void setEnvioID(long envioID) {
+        this.envioID = envioID;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    public String getEstadoEnvio() {
+        return estadoEnvio;
+    }
+    public void setEstadoEnvio(String estadoEnvio) {
+        this.estadoEnvio = estadoEnvio;
+    }
+    public int getPeriodicidad() {
+        return periodicidad;
+    }
+    public void setPeriodicidad(int periodicidad) {
+        this.periodicidad = periodicidad;
+    }
+    
+    
+}
