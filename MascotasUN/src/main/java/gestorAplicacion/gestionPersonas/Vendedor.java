b7e01e496bebc3ab1febe6f87a154fde5c50b073
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2022-10-22 15:36:57 -0500

    Arreglando métodos

--- MascotasUN/src/main/java/gestorAplicacion/gestionPersonas/Vendedor.java
+++ MascotasUN/src/main/java/gestorAplicacion/gestionPersonas/Vendedor.java
@@ -61,7 +61,8 @@ public class Vendedor extends Persona implements Serializable {
     public void setEsAdmin(boolean esAdmin) {
         this.esAdmin = esAdmin;
     }
-
+    
+    //Métodos
     public boolean validarCredenciales(String usuario, String contrasena){
         dict();
         for (Entry<String, String> entry : usuarios.entrySet()) {
@@ -72,13 +73,4 @@ public class Vendedor extends Persona implements Serializable {
         return false;
     }
 
-    @Override
-    public void agregarCuentaBancaria() {
-
-    }
-
-    @Override
-    public void eliminarCuentaBancaria() {
-
-    }
 }
