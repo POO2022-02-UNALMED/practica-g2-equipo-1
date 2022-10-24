089c09ea22bdb513a5c03f947afa613c16aa5453
Alejandra Uribe <aluribes@unal.edu.co>
2022-10-23 20:13:34 -0500

    Agregando rama en compra inventario
    Co-Authored-By: Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>

--- MascotasUN/src/main/java/uiMain/funcionalidades/CompraInventario.java
+++ MascotasUN/src/main/java/uiMain/funcionalidades/CompraInventario.java
@@ -1,12 +1,30 @@
 package uiMain.funcionalidades;
 
+import java.util.Scanner;
+
 import gestorAplicacion.gestionVentas.Tienda;
 
 public class CompraInventario {
-	public static  void funcionalidad() {
-		System.out.println("Este es su inventario actual");
-		//System.out.println(Tienda.getInventarioProductos());
-		Tienda.imprimirOrdenado(Tienda.getValoresOrdenados(), Tienda.getInventarioProductos());
+	public static void funcionalidad() {
+		System.out.println("¿Quieres ver el inventario completo o los productos en riesgo?");
+		Scanner input1 = new Scanner(System.in);
+        int opcion;
+        do {
+        	System.out.println("	1. Ver inventario completo");
+            System.out.println("	2. Ver productos con bajo stock");
+            System.out.print("Indique su eleccion : ");
+            opcion = input1.nextInt();
+            
+            switch (opcion) {
+            case 1:
+            	System.out.println("Este es su inventario actual");
+            	Tienda.imprimirOrdenado(Tienda.getValoresOrdenados(), Tienda.getInventarioProductos()); break;
+            case 2: 
+            	//funcionalidades.CompraInventario.funcionalidad() ; break;
+            }
+        }  while (opcion != 2);
+ 
+		
 		// Imprimir lista de inventario ordenado por cantidadComprada
 		System.out.println("¿Desea comprar inventario?");
 	}
