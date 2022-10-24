4582658165597ca1d745ca43704ca4394d90a307
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-10-24 18:41:20 -0500

    cambios en gestion
    Co-Authored-By: Felipe López  <74198982+Felo-afk@users.noreply.github.com>
    Co-Authored-By: Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
    Co-Authored-By: Alejandra Uribe Sierra <92702107+aluribes@users.noreply.github.com>
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>

--- MascotasUN/src/main/java/uiMain/gestion/gestionProveedores/VerProveedor.java
+++ MascotasUN/src/main/java/uiMain/gestion/gestionProveedores/VerProveedor.java
@@ -1,5 +1,6 @@
 package uiMain.gestion.gestionProveedores;
 
+import gestorAplicacion.gestionPersonas.Proveedor;
 import gestorAplicacion.gestionPersonas.Usuario;
 
 import java.util.ArrayList;
@@ -11,36 +12,30 @@ public class VerProveedor {
     public static void verProveedor(){
         Scanner input =new Scanner(System.in);
         do {
-            System.out.println("Estas a punto de ver tus usuarios");
-            System.out.println("1. Para ver un usuario en especifico");
-            System.out.println("2. Para ver la lista completa de tus usuarios");
+            System.out.println("Estas a punto de ver tus proveedores");
+            System.out.println("1. Para ver un proveedor en especifico");
+            System.out.println("2. Para ver la lista completa de tus proveedores");
             System.out.println("3. Volver");
-            opcion=Integer.parseInt(input.nextLine());
+            opcion=input.nextInt();
             switch (opcion){
                 case 1:
-                    System.out.println("Cuantos usuarios desea ver");
+                    System.out.println("Cuantos proveedores desea ver");
                     int cantidad=input.nextInt();
                     for(int i=1;i<=cantidad;i++){
-                        System.out.println("Ingrese el codigo del usuario que quiere ver: ");
+                        System.out.println("Ingrese el codigo del proveedor que quiere ver: ");
                         id=input.nextInt();
-                        Usuario usuario= Usuario.encontrarPersona(id);
-                        System.out.println(usuario.toString());
+                        Proveedor proveedor= Proveedor.encontrarPersona(id);
+                        System.out.println(proveedor.toString());
                         }
                     break;
                 case 2:
-                    System.out.println("Esta es la lista completa de tus usuarios");
-                    imprimirUsuarios(Usuario.getUsuarios());
+                    System.out.println("Esta es la lista completa de tus proveedores");
+                    System.out.println(Proveedor.getProveedores().toString());;
                     break;
                 case 3:
                     break;
             }
         } while(opcion != 3);
-    }
-    public static void imprimirUsuarios(ArrayList<Usuario> usuarios) {
-        for(Usuario i : usuarios) {
-            System.out.println("*******************************");
-            System.out.println("ID: "+ i.getPersonaId() +"\n"+ ". Nombre: " + i.getNombre() +"\n"+ ". Email: " + i.getEmail() +"\n"+". Telefono: " + i.getTelefono() +"\n"+". Compras: " + i.getCompras()+"\n"+". Mascotas: " + i.getMascotas()+"\n");
-        }
     }
 }
 
