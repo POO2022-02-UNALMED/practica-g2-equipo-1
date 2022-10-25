4735388be16469e9c2aa600b7c1f0c9ae81faef3
HinaraSM <hisanchezm@unal.edu.co>
2022-10-25 02:21:53 -0500

    menu recomendacion
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>
    Co-Authored-By: Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
    Co-Authored-By: Felipe López  <74198982+Felo-afk@users.noreply.github.com>
    Co-Authored-By: Alejandra Uribe Sierra <92702107+aluribes@users.noreply.github.com>

--- MascotasUN/src/main/java/uiMain/funcionalidades/RecomendacionesP.java
+++ MascotasUN/src/main/java/uiMain/funcionalidades/RecomendacionesP.java
@@ -9,61 +9,89 @@ import gestorAplicacion.gestionPersonas.Gato;
 import gestorAplicacion.gestionPersonas.Perro;
 import gestorAplicacion.gestionVentas.Producto;
 import gestorAplicacion.gestionVentas.Tienda;
+import uiMain.Principal;
 
 public class RecomendacionesP {
 	public static void funcionalidad() {
 		
-		//Se pregunta al vendedor que usuario requiere la recomendaci�n(El ID, o el nombre)
-		System.out.println("Ingrese el ID de usuario");
+		//Se pregunta al vendedor que usuario requiere la recomendacion(El ID, o el nombre)
+		System.out.println("¿Desea una recomendación personalizada");
 		Scanner input1 = new Scanner(System.in);
-		long opcion;
-		System.out.print("Indique su eleccion : ");
-        opcion = input1.nextInt();
-        Usuario usuario = null;
-		//Si este cliente existe, extraer la informaci�n, en caso contrario crear al cliente, con sus respectivas mascotas.
-		if (Tienda.getUsuarios().containsKey(opcion)) {
-			usuario = Tienda.getUsuarios().get(opcion);
-			System.out.println("Para cual de sus mascotas desea la recomendacion");
-			for (int i=0; i <usuario.getMascotas().size(); i++) {
-				Mascota mascota = usuario.getMascotas().get(i);
-				//Se imprime el IDMascota y su nombre si es perro
-				if (mascota.getClass() == Perro.class) {
-					System.out.println(((Perro)mascota).getMascotaID() + " " + ((Perro)mascota).getNombre());
-				}
-				//Se imprime el IDMascota y su nombre si es gato
-				if (mascota.getClass() == Gato.class) {
-					System.out.println(((Gato)mascota).getMascotaID() + " " + ((Gato)mascota).getNombre());
-				}
-			}		
-		}
-		
-		//Se ejecuta el m�todo que permite seleccionar la mascota.
-		Mascota mascotasel = RecomendacionesP.seleccionarMascota(usuario);
-		
-		
-		
-		//Crear nuevo cliente
-		//Metodo para crear cliente y luego crear y a�adir a sus mascotas
-		
-			
-		//Se genera la lista de recomendaciones para esa mascota tipoAlimento()
-		ArrayList<Producto> productosrecomendados = new ArrayList<Producto>();
-		//Lista si la mascota es Perro
-		if (mascotasel.getClass() == Gato.class) {
-			ArrayList<Producto> lista = RecomendacionesP.recomendacionGato(mascotasel);
-			productosrecomendados = lista;
-		}
-		//Lista si la mascota es Gato
-		if (mascotasel.getClass() == Perro.class) {
-			ArrayList<Producto> lista = RecomendacionesP.recomendacionPerro(mascotasel);
-			productosrecomendados = lista;
-		}
-		
-		//Se imprime el ToString de cada producto que aparezca en la lista
-		for (int i = 1; i < productosrecomendados.size() + 1; i++) {
-			System.out.println("Producto " + i + "\n" + productosrecomendados.get(i-1));
-		}	
+        int opcion;
+        do {
+        	System.out.println("	1. Si");
+            System.out.println("	2. No");
+            System.out.print("Indique su eleccion : ");
+            opcion = input1.nextInt();
+            switch (opcion) {
+            
+            // Se le muestra al vendedor el inventario completo, ordenado por cantidad
+            case 1:
+            	System.out.println("Ingrese el ID de usuario: ");
+        		Scanner input2 = new Scanner(System.in);
+        		long opcion2;
+                opcion2 = input1.nextInt();
+                Usuario usuario = null;
+        		//Si este cliente existe, extraer la informaci�n, en caso contrario crear al cliente, con sus respectivas mascotas.
+        		if (Tienda.getUsuarios().containsKey(opcion2)) {
+        			usuario = Tienda.getUsuarios().get(opcion2);
+        			System.out.println("Para cual de sus mascotas desea la recomendacion");
+        			for (int i=0; i <usuario.getMascotas().size(); i++) {
+        				Mascota mascota = usuario.getMascotas().get(i);
+        				//Se imprime el IDMascota y su nombre si es perro
+        				if (mascota.getClass() == Perro.class) {
+        					System.out.println(((Perro)mascota).getMascotaID() + " " + ((Perro)mascota).getNombre());
+        				}
+        				//Se imprime el IDMascota y su nombre si es gato
+        				if (mascota.getClass() == Gato.class) {
+        					System.out.println(((Gato)mascota).getMascotaID() + " " + ((Gato)mascota).getNombre());
+        				}
+        			}		
+        		}
+        		
+        		//Se ejecuta el m�todo que permite seleccionar la mascota.
+        		Mascota mascotasel = RecomendacionesP.seleccionarMascota(usuario);
+        		
+        		
+        		
+        		//Crear nuevo cliente
+        		//Metodo para crear cliente y luego crear y a�adir a sus mascotas
+        		
+        			
+        		//Se genera la lista de recomendaciones para esa mascota tipoAlimento()
+        		ArrayList<Producto> productosrecomendados = new ArrayList<Producto>();
+        		//Lista si la mascota es Perro
+        		if (mascotasel.getClass() == Gato.class) {
+        			ArrayList<Producto> lista = RecomendacionesP.recomendacionGato(mascotasel);
+        			productosrecomendados = lista;
+        		}
+        		//Lista si la mascota es Gato
+        		if (mascotasel.getClass() == Perro.class) {
+        			ArrayList<Producto> lista = RecomendacionesP.recomendacionPerro(mascotasel);
+        			productosrecomendados = lista;
+        		}
+        		
+        		//Se imprime el ToString de cada producto que aparezca en la lista
+        		for (int i = 1; i < productosrecomendados.size() + 1; i++) {
+        			System.out.println("Producto " + i + "\n" + productosrecomendados.get(i-1));
+        		}
+        		System.out.println("");
+        		funcionalidad();
+        	
+            	break;
+            	
+            // Se le muestra al vendedor los productos que están por agotarse, ordenado por cantidad
+            case 2:
+            	Principal.salirDelSistema();
+            	break;
+            }
+        }  while(opcion != 1 & opcion != 2);
 	}
+		
+		
+		
+		
+		
 	//Se pide seleccionar la mascota para la que desea la recomendaci�n en caso de no existir crear la nueva mascota.
 	public static Mascota seleccionarMascota(Usuario usuario) {
 		System.out.println("Ingrese el ID de su mascota"); 
