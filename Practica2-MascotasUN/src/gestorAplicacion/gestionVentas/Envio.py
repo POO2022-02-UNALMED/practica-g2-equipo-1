43015dc4e874fa41867cdbd2de94cbfa027b9b9c
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-11-19 00:13:38 -0500

    Clase envio lista
    Co-Authored-By: Felipe López  <74198982+Felo-afk@users.noreply.github.com>
    Co-Authored-By: Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>
    Co-Authored-By: Alejandra Uribe Sierra <92702107+aluribes@users.noreply.github.com>

---
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionVentas/Envio.py
@@ -0,0 +1,67 @@
+"""
+       Clase que permite la gestión de la 
+       funcionalidad de los envíos programados periódicos a un cliente
+    """
+    
+class Envio():
+    #Identificador unico de cada instancia
+    idEnvio=0
+    #Diccionario para guardar los envios registrados en el sistema
+    envios={}
+    #Constructor para la clase
+    def __init__(self,destino:str,estadoEnvio:str,periodicidad:int)->None:
+        Envio.idEnvio+=1
+        self.destino=destino
+        self.estadoEnvio=estadoEnvio
+        self.periodicidad =periodicidad
+        #Se guarda el objeto en un diccionario con su id como key
+        Envio.envios[self.idEnvio]=self 
+        self.factura=None   
+    #Métodos
+    
+    #Método que edita los atributos del envío    
+    def editarEnvio(self,destino:str,estadoEnvio:str,periodicidad:int):
+        self.estadoEnvio=estadoEnvio
+        self.destino=destino
+        self.periodicidad=periodicidad
+    
+    #Metodos de clase
+    @classmethod
+    def getEnvios(cls):
+        return cls.envios
+    @classmethod
+    def setEnvios(cls, facturas):
+        cls.facturas=facturas
+    #Getters y setters 
+    def getDestino(self):
+        return self.destino
+    
+    def setDestino(self,destino):
+        self.destino=destino
+        
+    def getFactura(self):
+        return self.factura
+        
+    def setFactura(self,factura):
+        self.factura=factura
+        
+    def getEstadoEnvio(self):
+        return self.estadoEnvio
+    
+    def setEstadoEnvio(self,estadoEnvio:str):
+        self.estadoEnvio=estadoEnvio
+    
+    def getPeriodicidad(self):
+        return self.periodicidad
+    
+    def setPeriodicidad(self,periodicidad):
+        self.periodicidad=periodicidad
+    #Metodo toString    
+    def __str__(self)->str:
+        return   "Identificador: "+ str(self.idEnvio) + "\n" \
+            + "Destino: "+ str(self.destino) + "\n" \
+            + "Estado envio: "+ str(self.estadoEnvio) + "\n" \
+            + "Periodicidad: "+ str(self.periodicidad) + "\n" \
+            + "Factura Asociada: "+ str(self.factura) + "\n"            
+        
+        
