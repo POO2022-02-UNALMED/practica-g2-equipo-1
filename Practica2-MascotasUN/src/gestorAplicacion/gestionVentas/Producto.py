4135398fd59d926e4c502df47d68e2a2624e2628
Alejandra Uribe <aluribes@unal.edu.co>
2022-11-20 16:15:32 -0500

    Producto listo
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>
    Co-Authored-By: Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
    Co-Authored-By: Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>

--- Practica2-MascotasUN/src/gestorAplicacion/gestionVentas/Producto.py
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionVentas/Producto.py
@@ -42,10 +42,33 @@ class Producto:
         
     # Métodos
     
-    # Método para imprimir los datos de un producto (toString)
+    # Método sobrecargado toString para imprimir los datos de un producto
+    def __str__(self) -> str:
+        return "Código: "+ str(self.productoID) + "\n" \
+            + "Producto: "+ str(self.nombre) + "\n" \
+            + "Fecha de Vencimiento: "+ str(self.fechaVencimiento) + "\n" \
+            + "Descripción: "+ str(self.descripcion) + "\n" \
+            + "Precio: "+ str(self.precioVenta) + "\n"
+            
     # Método encargado de editar los atributos de un producto
+    def editarProducto(self, nombre, precioVenta, cantidadComprada):
+        self.nombre = nombre
+        self.precioVenta = precioVenta
+        self.cantidadComprada = cantidadComprada
+    
     # Método que elimina un producto de la lista de productos a comprar
+    @classmethod
+    def eliminarProducto(cls, productoID):
+        for producto in cls.productos:
+            if producto.productoID == productoID:
+                cls.productos.remove(producto)
+                return True
+        return False
+    
     # Método que agrega un producto a la lista de productos a comprar
+    @classmethod
+    def agregarProducto(cls, producto):
+        cls.productos.append(producto)
     
     # Setters y getters
     
