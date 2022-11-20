4bd878a877011356dcfa80245cefbd559fc6c352
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-11-20 18:32:02 -0500

    Cambios

--- Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Proveedor.py
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Proveedor.py
@@ -1,88 +1,92 @@
 """Clase que crea los objetos tipo Proveedor, los cuales interactúan con la tienda
 vendiéndole el inventario requerido"""
+
+
 class Proveedor(Persona):
-    #Diccionario para almacenar las instancias de tipo proveedor
-    proveedores={}
+    # Diccionario para almacenar las instancias de tipo proveedor
+    proveedores = {}
     """
        public Proveedor() {
-    }""" 
-    
-    #Constructor de la clase
-    def __init__(self,personaId:int,nombre:str,email:str,telefono:int,stock:bool)->None:
-        cuenta=None
-        #Hacemos referencia al construtor de la clase padre
-        super().__init__(self,personaId,nombre,email,telefono,cuenta)
-        #Atributos
-        producto=None
-        pedidos=None
-        factura=None
-        self.stock=stock
-        #Se guarda el objeto en un diccionario con su id como key
-        Proveedor.proveedores[self.personaId]=self    
-    
-    #Metodos
-    
-    #Método que agrega una factura al proveedor a la lista de pedidos enviados
-    def agregarFactura(self,factura):
-        if self.factura==None:
-            self.pedidos=None
-        self.pedidos.append(factura)    
-   
-    #Metodo toString() que se encarga de mostrar atributos determinados de la clase proveedor    
-    def __str__(self)->str:
-        return   "~ ID # "+ str(self.personaId) + "\n" \
-            + "~ Nombre: "+ str(self.nombre) + "\n" \
-            + "~ Email: "+ str(self.email) + "\n" \
-            + "~ Telefono: "+ str(self.telefono) + "\n" \
-            + "~ Productos: "+ str(len(self.producto)) + "\n" \
-            + "~ Stock: "+ str(self.stock) + "\n"   
-    
-    #Método que encuentra al proveedor en la lista de proveedores existentes y lo retorna, dado su ID        
+    }"""
+
+    # Constructor de la clase
+    def __init__(self, personaId: int, nombre: str, email: str, telefono: int, stock: bool) -> None:
+        cuenta = None
+        # Hacemos referencia al construtor de la clase padre
+        super().__init__(self, personaId, nombre, email, telefono, cuenta)
+        # Atributos
+        producto = None
+        pedidos = None
+        factura = None
+        self.stock = stock
+        # Se guarda el objeto en un diccionario con su id como key
+        Proveedor.proveedores[self.personaId] = self
+
+        # Metodos
+
+    # Método que agrega una factura al proveedor a la lista de pedidos enviados
+    def agregarFactura(self, factura):
+        if self.factura == None:
+            self.pedidos = None
+        self.pedidos.append(factura)
+
+        # Metodo toString() que se encarga de mostrar atributos determinados de la clase proveedor
+
+    def __str__(self) -> str:
+        return "~ ID # " + str(self.personaId) + "\n" \
+               + "~ Nombre: " + str(self.nombre) + "\n" \
+               + "~ Email: " + str(self.email) + "\n" \
+               + "~ Telefono: " + str(self.telefono) + "\n" \
+               + "~ Productos: " + str(len(self.producto)) + "\n" \
+               + "~ Stock: " + str(self.stock) + "\n"
+
+        # Método que encuentra al proveedor en la lista de proveedores existentes y lo retorna, dado su ID
+
     def encontrarPersona(self):
         for p in self.proveedores:
-            if p.getPersonaId()==self.personaId:
+            if p.getPersonaId() == self.personaId:
                 return p
         return None
-    
-    #Metodo que elimina a un proveedor de la lista de provedores
-    def eliminarProveedor(self):
-        for p in self.proveedores:
-            if p.getPersonaId()==self.personaId:
-                Proveedor.proveedores.pop(self.personaId)
-                return Proveedor.proveedores
-        return None 
-    
-    #Metodos de clase
+
+    # Metodo que elimina a un proveedor de la lista de provedores
+    def eliminarProveedor(cls):
+        for p in cls.proveedores:
+            if p.getPersonaId() == cls.personaId:
+                cls.proveedores.pop(cls.personaId)
+                return cls.proveedores
+        return None
+
+        # Metodos de clase
+
     @classmethod
     def getProveedores(cls):
         return cls.proveedores
-    
+
     @classmethod
     def setProveedores(cls, proveedores):
-        cls.proveedores=proveedores
-        
-    #Getters y setters 
+        cls.proveedores = proveedores
+
+    # Getters y setters
     def getFactura(self):
         return self.factura
-    
-    def setFactura(self,factura):
-        self.factura=factura
+
+    def setFactura(self, factura):
+        self.factura = factura
 
     def getProductos(self):
         return self.producto
-    
-    def setProductos(self,producto):
-        self.producto=producto
- 
+
+    def setProductos(self, producto):
+        self.producto = producto
+
     def getPedidos(self):
         return self.pedidos
-    
-    def setPedidos(self,pedidos):
-        self.pedidos=pedidos
- 
+
+    def setPedidos(self, pedidos):
+        self.pedidos = pedidos
+
     def isStock(self):
         return self.stock
-    
-    def setStock(self,stock):
-        self.stock=stock          
-                
+
+    def setStock(self, stock):
+        self.stock = stock
--- Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Usuario.py
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Usuario.py
@@ -1,59 +1,64 @@
 """Clase que crea y gestiona los objetos tipo Usuario, los cuales interactúan
   con la tienda comprando productos. Hereda de la clase Persona"""
-class Usuario(Persona):
-    usuarios={}
-     
-     #Constructor de la clase
-    def __init__(self,personaId:int,nombre:str,email:str,telefono:int,direccion:str)->None:
-        cuenta=None
-        #Hacemos referencia al construtor de la clase padre
-        super().__init__(self,personaId,nombre,email,telefono,cuenta)
-        #Atributos
-        mascotas=None
-        compras=None
-        factura=None
-        self.direccion=direccion
-        #Se guarda el objeto en un diccionario con su id como key
-        Usuario.usuarios[self.personaId]=self    
-      
-    """Revisar sobrecarga de constructor"""
-    #Métodos
 
-	#Metodo toString() sobreescrito que se encarga de mostrar atributos 
- #   determinados de la clase usuario
-    def __str__(self)->str:
-        return   "~ ID # "+ str(self.personaId) + "\n" \
-            + "~ Nombre: "+ str(self.nombre) + "\n" \
-            + "~ Email: "+ str(self.email) + "\n" \
-            + "~ Telefono: "+ str(self.telefono) + "\n" \
-            + "~ Direccion: "+ str(self.direccion) + "\n" \
-            + "~ Mascotas: "+ str(self.mascotas) + "\n" \
-            + "~ Compras: "+ str(self.compras) + "\n" 
-    
-    #Método que encuentra al usuario en la lista de usuarios existentes y lo retorna, dado su ID
+
+class Usuario(Persona):
+    usuarios = {}
+
+    # Constructor de la clase
+    def __init__(self, personaId: int, nombre: str, email: str, telefono: int, direccion: str) -> None:
+        cuenta = None
+        # Hacemos referencia al construtor de la clase padre
+        super().__init__(self, personaId, nombre, email, telefono, cuenta)
+        # Atributos
+        mascotas = None
+        compras = None
+        factura = None
+        self.direccion = direccion
+        # Se guarda el objeto en un diccionario con su id como key
+        Usuario.usuarios[self.personaId] = self
+
+    """Revisar sobrecarga de constructor"""
+
+    # Métodos
+
+    # Metodo toString() sobreescrito que se encarga de mostrar atributos
+    #   determinados de la clase usuario
+    def __str__(self) -> str:
+        return "~ ID # " + str(self.personaId) + "\n" \
+               + "~ Nombre: " + str(self.nombre) + "\n" \
+               + "~ Email: " + str(self.email) + "\n" \
+               + "~ Telefono: " + str(self.telefono) + "\n" \
+               + "~ Direccion: " + str(self.direccion) + "\n" \
+               + "~ Mascotas: " + str(self.mascotas) + "\n" \
+               + "~ Compras: " + str(self.compras) + "\n"
+
+        # Método que encuentra al usuario en la lista de usuarios existentes y lo retorna, dado su ID
+
     def encontrarPersona(self):
         for u in self.usuarios:
-            if u.getPersonaId()==self.personaId:
+            if u.getPersonaId() == self.personaId:
                 return u
         return None
 
-    #Método que agrega una factura al usuario dentro de la lista de compras realizadas
-    def agregarFactura(self,factura):
-        if self.factura==None:
-            self.compras=None
-        self.compras.append(factura)   
-        
-    #Método que registra una nueva mascota en la lista de mascotas pertenecientes al usuario
-    def agregarMascota(self,mascota):
-        if mascota!=None:
+    # Método que agrega una factura al usuario dentro de la lista de compras realizadas
+    def agregarFactura(self, factura):
+        if self.factura == None:
+            self.compras = None
+        self.compras.append(factura)
+
+        # Método que registra una nueva mascota en la lista de mascotas pertenecientes al usuario
+
+    def agregarMascota(self, mascota):
+        if mascota != None:
             self.mascotas.append(mascota)
 
-    #Método que permite eliminar un usuario desde la gestión de usuarios de la tienda
-    def eliminarUsuario(self):
-        for u in self.usuarios:
-            if u.getPersonaId()==self.personaId:
-                Usuario.usuarios.pop(self.personaId)
-                return Usuario.usuarios
+    # Método que permite eliminar un usuario desde la gestión de usuarios de la tienda
+    def eliminarUsuario(cls):
+        for usuario in cls.usuarios:
+            if usuario.getPersonaId() == cls.personaId:
+                cls.usuarios.pop(cls.personaId)
+                return cls.usuarios
         return None
 
         # Metodos de clase
@@ -62,30 +67,30 @@ class Usuario(Persona):
             return cls.usuarios
 
         @classmethod
-        def setUsuarios(cls,usuarios):
+        def setUsuarios(cls, usuarios):
             cls.usuarios = usuarios
 
         # Getters y setters
         def getDireccion(self):
             return self.direccion
 
-        def setDireccion(self,direccion):
-            self.direccion=direccion
+        def setDireccion(self, direccion):
+            self.direccion = direccion
 
         def getMascotas(self):
             return self.mascotas
 
-        def setMascotas(self,mascotas):
-            self.mascotas=mascotas
+        def setMascotas(self, mascotas):
+            self.mascotas = mascotas
 
         def getCompras(self):
             return self.compras
 
-        def setCompras(self,compras):
-            self.compras=compras
+        def setCompras(self, compras):
+            self.compras = compras
 
         def getFactura(self):
             return self.factura
 
-        def setFactura(self,factura):
+        def setFactura(self, factura):
             self.factura = factura
