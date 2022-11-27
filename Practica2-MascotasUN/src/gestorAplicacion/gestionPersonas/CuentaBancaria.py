41ada15207650eb9ecd733a74528791c6258e5df
Alejandra Uribe <aluribes@unal.edu.co>
2022-11-26 23:30:02 -0500

    Cuenta Bancaria arreglado
    Co-Authored-By: Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
    Co-Authored-By: Felipe López  <74198982+Felo-afk@users.noreply.github.com>
    Co-Authored-By: Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>

--- Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/CuentaBancaria.py
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/CuentaBancaria.py
@@ -1,75 +1,73 @@
-class CuentaBancaria:	
-	
-	_cuentasBancarias = []
-	_credenciales = {}
-
-	#Constructor
-	def __init__(self, nroCuenta, saldo, pin = None):
-		self._nroCuenta = nroCuenta
-		self._saldo = saldo
-		self._pin = pin
-		CuentaBancaria._credenciales[nroCuenta] = pin
-	
-	#Métodos
-	
-	# Metodo para retirar dinero de la cuenta propia. Ingresamos el monto a retirar y el pin de la cuenta. 
-	# Se utiliza en el método transferir()
-	def retirar(self, monto, pin) :
-		if (monto <= self._saldo and self._validarCredenciales(pin)==True):
-			self.setSaldo(self.getSaldo() - monto)
-		
-	#Metodo para depositar dinero en la cuenta propia. Ingresamos el monto a depositar.
-	#Se utiliza en el método transferir() de la clase actual y en realizarCompra() de la clase Factura*/
-	def depositar(self, monto):
-		self.setSaldo(monto + self.getSaldo())
-
-	#Metodo para tranferir dinero a otra cuenta. Requiere ingresar la cuenta a la que se depositará el dinero, el monto a depositar, y el pin de la cuenta*/
-	def tranferir(self, cuenta, monto, pin):
-		if (monto <= self.getSaldo() and self.validarCredenciales(pin)== True):
-			self.retirar(monto, pin)
-			if (isinstance(cuenta, CuentaBancaria)==True):
-			    cuenta.depositar(monto)	
-
-    #Getter y setter utilizados para la serializacion y deserializacion de objetos
-    @classmethod
-	def getCuentasBancarias(cls):
-		return cls._cuentasBancarias
+class CuentaBancaria:
     
-	@classmethod
-	def setNroCuenta(cls, nroCuenta):
-		cls._nroCuenta = nroCuenta
+    _cuentasBancarias = []
+    _credenciales = {}
+    
+    def __init__(self, nroCuenta, saldo, pin=None):
+        self._nroCuenta = nroCuenta
+        self._saldo = saldo
+        self._pin = pin
+        self._credenciales[nroCuenta] = pin
+        self._cuentasBancarias.append(self)
+        
+    # Métodos
+    
+    # Metodo para retirar dinero de la cuenta propia. Ingresamos el monto a retirar y el pin de la cuenta. 
+	# Se utiliza en el método transferir()
+ 
+    def retirar(self, monto, pin):
+        if monto <= self._saldo and self._validarCredenciales(pin) == True:
+            self.setSaldo(self.getSaldo() - monto)
+            
+    # Metodo para depositar dinero en la cuenta propia. Ingresamos el monto a depositar.
+	# Se utiliza en el método transferir() de la clase actual y en realizarCompra() de la clase Factura
+
+    def depositar(self, monto):
+        self.setSaldo(monto + self.getSaldo())
+        
+    # Metodo para tranferir dinero a otra cuenta. Requiere ingresar la cuenta a la que se depositará el dinero, 
+    # el monto a depositar, y el pin de la cuenta
+    
+    def transferir(self, cuenta, monto, pin):
+        if monto <= self.getSaldo() and self._validarCredenciales(pin) == True:
+            self.retirar(monto, pin)
+            if isinstance(cuenta, CuentaBancaria) == True:
+                cuenta.depositar(monto)
+                
+    # Getter y setter utilizados para la serializacion y deserializacion de objetos
     
     @classmethod
     def getCuentasBancarias(cls):
-		return cls._cuentasBancarias
-
+        return cls._cuentasBancarias
+    
     @classmethod
-	def setCuentasBancarias(cls, cuentasBancarias):
-	    cls._cuentasBancarias=cuentasBancarias
-
-    #Getters y setters
-	def getNroCuenta(self) :
-		return self._nroCuenta
-	
-	def setNroCuenta(self, nroCuenta):
-		self._nroCuenta = nroCuenta
-	
-	def getSaldo(self):
-		return self._saldo
-
-	def setSaldo(self, saldo):
-		self._saldo = saldo
-	
-	def getPin(self):
-		return self._pin
-	
-	def setPin(self, pin):
-		self._pin = pin
-
-	@classmethod
-	def getCredenciales(cls):
-		return cls._credenciales
-
+    def setCuentasBancarias(cls, cuentasBancarias):
+        cls._cuentasBancarias = cuentasBancarias
+        
+    # Getters y setters
+        
+    def getNroCuenta(self):
+        return self._nroCuenta
+    
+    def setNroCuenta(self, nroCuenta):
+        self._nroCuenta = nroCuenta
+        
+    def getSaldo(self):
+        return self._saldo
+    
+    def setSaldo(self, saldo):
+        self._saldo = saldo
+        
+    def getPin(self):
+        return self._pin
+    
+    def setPin(self, pin):
+        self._pin = pin
+        
     @classmethod
-	def setCredenciales(cls, credenciales):
-		cls.credenciales = credenciales
+    def getCredenciales(cls):
+        return cls._credenciales
+    
+    @classmethod
+    def setCredenciales(cls, credenciales):
+        cls._credenciales = credenciales
