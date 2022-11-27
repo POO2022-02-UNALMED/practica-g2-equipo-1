2450950e26ff81b553b685da71d722b0a3d2dd93
HinaraSM <hisanchezm@unal.edu.co>
2022-11-26 20:48:41 -0500

    vendedor
    Co-Authored-By: Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
    Co-Authored-By: Felipe López  <74198982+Felo-afk@users.noreply.github.com>
    Co-Authored-By: Alejandra Uribe Sierra <92702107+aluribes@users.noreply.github.com>
    Co-Authored-By: jtobonz <92930935+jtobonz@users.noreply.github.com>

--- Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Vendedor.py
+++ Practica2-MascotasUN/src/gestorAplicacion/gestionPersonas/Vendedor.py
@@ -1,145 +1,107 @@
 #Clase que crea y gestiona los objetos tipo Vendedor, los cuales interactúan directamente con las funcionalidades
- #de la aplicación. Hereda de la clase abstracta Persona. Existen dos hashmap, el primero (usuarios) se encarga de
+ #de la aplicación. Hereda de la clase abstracta Persona. Existen dos diccionarios, el primero (usuarios) se encarga de
  #obtener usuarios con usuario y contrasena, el segundo (cantidadVentas) muestra el string del usuario y las ventas
  #del mismo
 
-from gestorAplicacion.gestionVentas import Factura
+from gestorAplicacion.gestionPersonas.Persona import Persona
+#from gestorAplicacion.gestionVentas.Factura import Factura
 
 
 class Vendedor(Persona):
     _comision=0.05
     _vendedores=[]
-	#//Serializador vendedor
-	#private static final long serialVersionUID = 1L;
-	#private static final double comision = 0.05;
-	#private static ArrayList<Vendedor> vendedores = new ArrayList<>();
+    usuarios={}
+    cantidadVentas={}
     
-    #Atributos
-    #private ArrayList<Factura> ventas=new ArrayList<Factura>();
-    #private String usuario;
-    #private String contrasena;
-    #public static HashMap<String, String> usuarios = new HashMap<String, String>(); // Diccionario vendedores usuario:contraseña
-    #public static HashMap<String, Integer> cantidadVentas = new HashMap<String, Integer>(); // Diccionario vendedores usuario:ventas
-
     #Constructor
-    def __init__(self,personaId, nombre, email, telefono, venta, usuario, contrasena, cuenta):
+    def __init__(self,personaId, nombre, email, telefono, ventas, usuario, contrasena, cuenta):
         super().__init__(personaId, nombre, email, telefono,cuenta)
-        self._venta = venta
+        self._ventas = ventas
         self._usuario = usuario
         self._contrasena = contrasena
         
         Vendedor._vendedores.append(self)
-    
-    public Vendedor(long personaId, String nombre, String email, int telefono, ArrayList<Factura> ventas,
-    		String usuario, String contrasena, CuentaBancaria cuenta) {
-        super(personaId, nombre, email, telefono,cuenta);
-        this.ventas = ventas;
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-        vendedores.add(this);
-    }
 
-    //Métodos
+    #Métodos
     
-	// Método para ordenar el HashMap del vendedor por la cantidad de ventas 
-	public static SortedSet<Integer> getValoresOrdenados() {
-        SortedSet<Integer> valores = new TreeSet<Integer>(cantidadVentas.values());
-        return valores; 
-    }
+	# Método para ordenar el HashMap del vendedor por la cantidad de ventas 
+    @classmethod
+    def getValoresOrdenados(cls):
+        valores= sorted(cls.cantidadVentas.values())
+        return  valores
 
-    // Método sobreescrito toString()
-    @Override
-    public String toString() {
-        return  "~ ID #"+ personaId + "\n"+
-                "~ Nombre = " + nombre +"\n"+
-                "~ Email = " + email + "\n"+
-                "~ Telefono = " +telefono+ "\n"+
-                "~ Usuario = " +usuario+ "\n"+
-                "~ Ventas = " +ventas+ "\n";
-    }
+    #Método sobreescrito toString()  
+    def __str__(self):
+        cadena="~ ID #"+ self.personaId + "\n", 
+        "~ Nombre = " + self.nombre +"\n", 
+        "~ Email = " + self.email + "\n",
+        "~ Telefono = " + self.telefono+ "\n",
+        "~ Usuario = " +self.usuario+ "\n",
+        "~ Ventas = " +self.ventas+ "\n",
+        return cadena
     
-    // Método que agrega al vendedor a la lista de vendedores
-    public static void agregarVendedor(Vendedor vendedor){
-        vendedores.add(vendedor);
-    }
+    #Método que agrega al vendedor a la lista de vendedores
+    @classmethod
+    def agregarVendedor(cls, vendedor):
+        cls._vendedores.append(vendedor)
 
-    // Método que agrega una factura al vendedor dentro de la lista de ventas realizadas
-    @Override
-    public void agregarFactura(Factura f){
-        ventas.add(f);
-    }
+    #Método que agrega una factura al vendedor dentro de la lista de ventas realizadas
+    def agregarFactura(self,f):
+        self._ventas.append(f)
 
-    // Método que permite validar que las credenciales ingresadas por el vendedor al iniciar sesión sean correctas
-    public static boolean validarCredenciales(String usuario, String contrasena){
-        for (Entry<String, String> entry : usuarios.entrySet()) {
-            if (usuario.equals(entry.getKey()) & contrasena.equals(entry.getValue())){
-                return true;
-            }
-        }
-        return false;
-    }
-    // Método que encuentra al vendedor en la lista de vendedores existentes y lo retorna, dado su ID
-    public static Vendedor encontrarPersona(long personaId){
-        for(Vendedor p: vendedores){
-            if(p.getPersonaId()== personaId){
-                return p;
-            }
-        }
-        return null;
-    }
+    #Método que permite validar que las credenciales ingresadas por el vendedor al iniciar sesión sean correctas
+    @classmethod
+    def validarCredenciales(cls,usuario, contrasena):
+        for i in cls.usuarios.keys():
+            if usuario==i and contrasena==cls.usuarios.get(i):
+                return True
+        return False
+    
+    #Método que encuentra al vendedor en la lista de vendedores existentes y lo retorna, dado su ID
+    @classmethod
+    def encontrarPersona(cls, personaId):
+        for p in cls._vendedores:
+            if(p.getPersonaId()== personaId):
+                return p
+        return None
 
-    //Getters y setters
+    #Getters y setters
     
-    public static ArrayList<Vendedor> getVendedores() {
-        return vendedores;
-    }
+    def getVendedores(cls):
+        return cls._vendedores
     
-    public static void setVendedores(ArrayList<Vendedor> vendedores) {
-        Vendedor.vendedores=vendedores;
-    }
+    def setVendedores(cls,vendedores):
+        cls._vendedores=vendedores
     
-    public ArrayList<Factura> getVentas() {
-        return ventas;
-    }
-    
-    public void setVentas(ArrayList<Factura> ventas) {
-        this.ventas = ventas;
-    }
-    
-    public String getUsuario() {
-        return usuario;
-    }
-    
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-    
-    public String getContrasena() {
-        return contrasena;
-    }
-    
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
+    def getVentas(self):
+        return self._ventas
 
-	public static HashMap<String, String> getUsuarios() {
-		return usuarios;
-	}
+    def setVentas(self,ventas):
+        self._ventas = ventas
+        
+    def getUsuario(self):
+        return self._usuario
 
-	public static void setUsuarios(HashMap<String, String> usuarios) {
-		Vendedor.usuarios = usuarios;
-	}
+    def setUsuario(self,usuario):
+        self._usuario = usuario
+        
+    def getContrasena(self):
+        return self._contrasena
 
-	public static HashMap<String, Integer> getCantidadVentas() {
-		return cantidadVentas;
-	}
+    def setContrasena(self,contrasena):
+        self._contrasena = contrasena
 
-	public static void setCantidadVentas(HashMap<String, Integer> cantidadVentas) {
-		Vendedor.cantidadVentas = cantidadVentas;
-	}
-	
-    public static double getComision() {
-		return comision;
-	}
-	
-}
+    def getUsuarios(cls):
+        return cls._usuarios
+    
+    def setUsuarios(cls,usuarios):
+        cls._usuarios=usuarios
+        
+    def getCantidadVentas(cls):
+        return cls.cantidadVentas
+    
+    def setCantidadVentas(cls,cantidadVentas):
+        cls.cantidadVentas=cantidadVentas
+        
+    def getComision(cls):
+        return cls._comision
